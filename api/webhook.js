5984db1d3538cdaa9e2181de6bc63ce7ceb52c40
Amo7977 <154580467+Amo7977@users.noreply.github.com>
2025-02-18 05:00:51 +0900

    Delete api/webhook.js

--- api/webhook.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    const webhookUrl = process.env.WEBHOOK_URL;
-
-    if (!webhookUrl) {
-        return res.status(500).json({ error: 'Webhook URL is not set.' });
-    }
-
-    res.status(200).json({ webhookUrl });
-}
